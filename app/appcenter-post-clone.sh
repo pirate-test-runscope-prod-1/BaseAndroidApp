7eeca8cd8a0a93d6f5f10cf7c78a15975fac6c21
Yury Nevalenny <nevalenny@gmail.com>
2019-04-08 17:44:04 +0300

    Update bintray tracking script

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -3,7 +3,7 @@ LOG=bintray.log
 SLOW_LOG=bintray_slow.log
 SLOW_RESPONSE=5.000
 
-for i in {1..100}
+for i in {1..1000}
 do 
     OUTPUT="$(curl https://jcenter.bintray.com/com/android/tools/lint/lint-checks/25.1.3/lint-checks-25.1.3.pom -vs -w '\ntime_namelookup=%{time_namelookup}\ntime_appconnect=%{time_appconnect}\ntime_connect=%{time_connect}\ntime_redirect=%{time_redirect}\ntime_pretransfer=%{time_pretransfer}\ntime_starttransfer=%{time_starttransfer}\ntime_total=%{time_total}\n\n' &> /dev/stdout | tee -a ${LOG})"
     TIME_TOTAL=$(sed -n 's/time_total=//p' <<< "$OUTPUT")
